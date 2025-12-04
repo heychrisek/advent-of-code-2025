f3d374a956e469b9d33d93344888afb3fa3a9e55
Chris Eaves-Kohlbrenner <chrisek@thatch.ai>
2025-12-04 10:28:13 -0500

    cursor-assisted improvements and ruby idioms

--- d04.rb
+++ d04.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'pry'
-
 class D04
   def initialize(input)
     @input = input
@@ -9,55 +7,63 @@ class D04
   end
 
   def part1
-    grid = @original_grid.map(&:dup)
+    grid = duplicate_grid
     find_accessible_cells(grid).length
   end
 
   def part2
-    grid = @original_grid.map(&:dup)
-    accessible_cells_count = 0
+    grid = duplicate_grid
+    count = 0
     loop do
       accessible_cells = find_accessible_cells(grid)
       break if accessible_cells.empty?
 
-      accessible_cells.each do |row_idx, col_idx|
-        grid[row_idx][col_idx] = '.'
-      end
-      accessible_cells_count += accessible_cells.length
+      accessible_cells.each { |row_i, col_i| grid[row_i][col_i] = '.' }
+      count += accessible_cells.length
     end
-    accessible_cells_count
+    count
   end
 
   def find_accessible_cells(grid)
-    accessible_cells = []
-    for row_idx in 0...grid.length
-      row = grid[row_idx]
-      for col_idx in 0...row.length
-        char = row[col_idx]
+    grid.each_with_index.flat_map do |row, row_i|
+      row.each_char.with_index.filter_map do |char, col_i|
         next unless char == '@'
+        next unless can_access?(row_i, col_i, grid)
 
-        accessible_cells << [row_idx, col_idx] if can_access?(row_idx, col_idx, grid)
+        [row_i, col_i]
       end
     end
-    accessible_cells
   end
 
   def parse
     @input.strip.split("\n")
   end
 
-  def can_access?(row_idx, col_idx, grid)
-    adjacent_count = 0
-    adjacent_count += 1 if row_idx > 0 && col_idx > 0 && grid[row_idx - 1][col_idx - 1] == '@'
-    adjacent_count += 1 if row_idx > 0 && grid[row_idx - 1][col_idx] == '@'
-    adjacent_count += 1 if row_idx > 0 && col_idx < grid[row_idx].length - 1 && grid[row_idx - 1][col_idx + 1] == '@'
-    adjacent_count += 1 if row_idx < grid.length - 1 && grid[row_idx + 1][col_idx] == '@'
-    adjacent_count += 1 if col_idx > 0 && grid[row_idx][col_idx - 1] == '@'
-    adjacent_count += 1 if row_idx < grid.length - 1 && col_idx > 0 && grid[row_idx + 1][col_idx - 1] == '@'
-    adjacent_count += 1 if col_idx < grid[row_idx].length - 1 && grid[row_idx][col_idx + 1] == '@'
-    if row_idx < grid.length - 1 && col_idx < grid[row_idx].length - 1 && grid[row_idx + 1][col_idx + 1] == '@'
-      adjacent_count += 1
+  def duplicate_grid
+    @original_grid.map(&:dup)
+  end
+
+  def can_access?(row_i, col_i, grid)
+    neighbor_count = count_neighbors(row_i, col_i, grid)
+    neighbor_count < 4
+  end
+
+  def count_neighbors(row_i, col_i, grid)
+    directions = [
+      [-1, -1], [-1, 0], [-1, 1],
+      [0, -1], [0, 1],
+      [1, -1], [1, 0], [1, 1]
+    ]
+
+    directions.count do |dr, dc|
+      new_row = row_i + dr
+      new_col = col_i + dc
+      in_bounds?(new_row, new_col, grid) && grid[new_row][new_col] == '@'
     end
-    adjacent_count < 4
+  end
+
+  def in_bounds?(row_i, col_i, grid)
+    row_i >= 0 && row_i < grid.length &&
+      col_i >= 0 && col_i < grid[row_i].length
   end
 end
