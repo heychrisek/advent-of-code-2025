da5301d4b9cbad50a48bfb81581f3d891d0fdea7
Chris Eaves-Kohlbrenner <chrisek@thatch.ai>
2025-12-05 09:37:46 -0500

    cursor ruby improvements

--- d05.rb
+++ d05.rb
@@ -10,32 +10,30 @@ class D05
   def part1
     parse
     @ingredient_ids.count do |ingredient_id|
-      @ranges.any? do |range|
-        range[0] <= ingredient_id && range[1] >= ingredient_id
+      @ranges.any? do |start, finish|
+        (start..finish).cover?(ingredient_id)
       end
     end
   end
 
   def part2
     parse
-    sum = 0
-    @ranges.each do |range|
-      sum += range[1] - range[0] + 1
+    @ranges.sum do |start, finish|
+      finish - start + 1
     end
-    sum
   end
 
   def parse
-    parsed_ranges, parsed_ingredients = @input.strip.split("\n\n")
-    parsed_ranges = parsed_ranges.strip.split("\n").map { |range| range.split('-').map(&:to_i) }
+    ranges_str, ingredients_str = @input.strip.split("\n\n")
+    parsed_ranges = ranges_str.strip.split("\n").map { |range| range.split('-').map(&:to_i) }
     @ranges = merge_ranges(parsed_ranges)
-    @ingredient_ids = parsed_ingredients.strip.split("\n").map(&:to_i)
+    @ingredient_ids = ingredients_str.strip.split("\n").map(&:to_i)
   end
 
   def merge_ranges(ranges)
     return [] if ranges.empty?
 
-    sorted = ranges.sort_by { |start, _end| start }
+    sorted = ranges.sort_by(&:first)
     merged = [sorted.first]
 
     sorted[1..].each do |current_start, current_end|
